872a341792ffcd3cbc384a95b81e8b3089a63303
jennifergthompson <jennifer.thompson@sjcd.edu>
2026-05-07 09:37:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const box = document.querySelector(".box");
   const colorButton = document.getElementById("changeColor");
   const toggleButton = document.getElementById("toggleImage");
- 
- 
+
   colorButton.addEventListener("click", () => {
-    bx.style.backgroundColor = "blue"; 
+    // Fixed: Changed 'bx' to 'box'
+    box.style.backgroundColor = "blue"; 
   });
- 
- 
- 
+
   let toggled = false;
   toggleButton.addEventListener("click", () => {
     if (toggled) {
       gallery.src = "./assets/image1.jpg";
       toggled = false;
     } else {
-      gallery.src = "./assets/image-does-not-exist.jpg"; 
+      // Note: Ensure this file actually exists or use a valid fallback URL
+      gallery.src = "./assets/image2.jpg"; 
       toggled = true;
     }
   });
- 
- 
- 
-  console.log("Page loaded!"
-});
- 
+
+  // Fixed: Added missing ')'
+  console.log("Page loaded!"); 
+}); // Fixed: Added missing '}'
